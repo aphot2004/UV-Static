f04361628f79b82d61eefb92b53584b6511c7bd2
aphot2004 <63328552+aphot2004@users.noreply.github.com>
2024-05-09 12:52:48 -0700

    Update and rename uv.config.js to ub.config.js

--- static/uv/ub.config.js
+++ static/uv/ub.config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare:'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/static/uv/uv.handler.js',
-    bundle: '/static/uv/uv.bundle.js',
-    config: '/static/uv/uv.config.js',
-    sw: '/static/uv/uv.sw.js',
+    handler: '/static/uv/ub.handler.js',
+    bundle: '/static/uv/ub.bundle.js',
+    config: '/static/uv/ub.config.js',
+    sw: '/static/uv/ub.sw.js',
 };
